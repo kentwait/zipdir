d1296e7dc40ed8b22633e87e6070b3db678f2aed
Kent Kawashima <kentkawashima@gmail.com>
2015-09-29 20:56:37 +0900

    Added special subdir marking functions

--- zipdir/api.py
+++ zipdir/api.py
@@ -1,5 +1,6 @@
 import subprocess as proc
 import datetime
+import os
 
 def run_7za(cmd_str: str):
     """Calls 7za using the passed string of arguments
@@ -7,14 +8,14 @@ def run_7za(cmd_str: str):
     return proc.check_output('7za {cmd_str}'.format(cmd_str=cmd_str), shell=True)
 
 
-def full_backup_dir(folder_name, save_path='.', strftime='%Y%m%d-%H%M%S'):
+def full_backup_dir(abs_dir_path, save_path='.', strftime='%Y%m%d-%H%M%S'):
     """Creates a full compressed backup of a folder using 7zip and tar.
     Ideally should be used when backing up a folder for the first time
 
     Parameters
     ----------
-    folder_name : path
-        Relative path to the folder to be compressed
+    abs_dir_path : path
+        Absolute path to the folder to be compressed
     save_path : path
         Save path of the base archive
     strftime : str
@@ -24,20 +25,21 @@ def full_backup_dir(folder_name, save_path='.', strftime='%Y%m%d-%H%M%S'):
     -------
     int
     """
+    folder_name = os.path.split(abs_dir_path)[-1]
     timestamp = datetime.datetime.now().strftime(strftime)
-    cmd_str = '7za a {save_path}/{folder_name}.{timestamp}.base.7z {folder_name}'.format(
-        save_path=save_path, folder_name=folder_name, timestamp=timestamp)
+    cmd_str = 'a {save_path}/{folder_name}.{timestamp}.base.7z {abs_dir_path}'.format(
+        save_path=save_path, folder_name=folder_name, timestamp=timestamp, abs_dir_path=abs_dir_path)
     return run_7za(cmd_str)
 
 
-def diff_backup_dir(folder_name, base_archive_filepath, save_path='.', strftime='%Y%m%d-%H%M%S'):
+def diff_backup_dir(abs_dir_path, base_archive_filepath, save_path='.', strftime='%Y%m%d-%H%M%S'):
     """Creates a differetial compressed backup of a folder using 7zip and tar.
     Must have a full backup in place already before a diff backup can be created.
 
     Parameters
     ----------
-    folder_name : path
-        Relative path of the folder being differentially backed-up
+    abs_dir_path : path
+        Absolute path of the folder being differentially backed-up
     base_archive_path : filename
         File path of the 7zip base (full) archive
     save_path : path
@@ -49,8 +51,10 @@ def diff_backup_dir(folder_name, base_archive_filepath, save_path='.', strftime=
     -------
     int
     """
+    folder_name = os.path.split(abs_dir_path)[-1]
     timestamp = datetime.datetime.now().strftime(strftime)
-    cmd_str = '7za u {base_archive_filepath} -ms=off -mx=9 -t7z -u- ' \
-              '-up0q3r2x2y2z0w2\!{save_path}/{folder_name}.{timestamp}.diff.7z {folder_name}'.format(
-        base_archive_filepath=base_archive_filepath, save_path=save_path, folder_name=folder_name, timestamp=timestamp)
+    cmd_str = 'u {base_archive_filepath} -ms=off -mx=9 -t7z -u- ' \
+              '-up0q3r2x2y2z0w2\!{save_path}/{folder_name}.{timestamp}.diff.7z {abs_dir_path}'.format(
+        base_archive_filepath=base_archive_filepath, save_path=save_path, folder_name=folder_name, timestamp=timestamp,
+        abs_dir_path=abs_dir_path)
     return run_7za(cmd_str)
--- zipdir/utils.py
+++ zipdir/utils.py
@@ -1,20 +1,21 @@
 import os
+import re
 
-def get_marked_dir_paths(dirpath, mark_filename='.compress_dir'):
+def get_marked_dirs(dir_path, mark_filename='.compress_dir'):
     """Get the absolute path of marked directories in a directory tree
     """
     marked_abs_paths = list()
-    for root, dirs, files in os.walk(dirpath):
+    for root, dirs, files in os.walk(dir_path):
         # path = root.split('/')
         if mark_filename in files:
             marked_abs_paths.append(os.path.abspath(root))
     return marked_abs_paths
 
 
-def remove_all_marks(dirpath, mark_filename='.compress_dir'):
+def remove_all_marks(dir_path, mark_filename='.compress_dir'):
     """Remove all marks in a directory tree
     """
-    for root, dirs, files in os.walk(dirpath):
+    for root, dirs, files in os.walk(dir_path):
         # path = root.split('/')
         if mark_filename in files:
             print('Found in {}'.format(root))
@@ -22,8 +23,37 @@ def remove_all_marks(dirpath, mark_filename='.compress_dir'):
 
 
 def mark_dirs(list_of_abs_paths: list, mark_filename='.compress_dir'):
-    """Places .compress_dir marks on each directory in the list
+    """Marks each directory in the list for compression
     """
     for path in list_of_abs_paths:
-        print(path)
+        print('Marking {}'.format(path))
         open('{}/{}'.format(path, mark_filename), 'a').close()
+
+def parse_dirs_file(fh):
+    return [line.replace('\n', '') for line in fh.readlines()]
+
+
+def mark_all_top_subdirs(root_dir, mark_filename='.compress_dir', exclude='^\.'):
+    assert os.path.isabs(root_dir)
+    abs_paths = list()
+    exclude_path = re.compile(str(exclude))
+    for path in os.listdir(root_dir):
+        if not exclude_path.search(path):
+            path = os.path.join(root_dir, path)
+            if os.path.isdir(path):
+                abs_paths.append(path)
+    mark_dirs(abs_paths, mark_filename=mark_filename)
+    return abs_paths
+
+
+def mark_all_end_subdirs(root_dir, mark_filename='.compress_dir', exclude='^\.'):
+    assert os.path.isabs(root_dir)
+    abs_paths = list()
+    exclude_path = re.compile(str(exclude))
+    for root, dirs, files in os.walk(root_dir):
+        if len(dirs) == 0:
+            if not exclude_path.search(os.path.split(root)[-1]):
+                if os.path.isdir(root):
+                    abs_paths.append(root)
+    mark_dirs(abs_paths, mark_filename=mark_filename)
+    return abs_paths
