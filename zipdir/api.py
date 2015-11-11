7efe160ec4dcd8aa3bc7df4872f04a99bf7755a8
kent <kentkawashima@gmail.com>
2015-11-11 18:52:41 +0900

    Docstrings update

--- zipdir/api.py
+++ zipdir/api.py
@@ -1,20 +1,35 @@
 import subprocess as proc
 import datetime
 import os
+import tarfile as tar
+# import shutil
+import re
 
-def run_7za(cmd_str: str):
+
+# 7zip based backup functions
+
+def run_7za(cmd_str: str, bin_path='7za'):
     """Calls 7za using the passed string of arguments
+
+    Parameters
+    ----------
+    cmd_str : str
+    bin_path : path
+
+    Returns
+    -------
+    process
     """
-    return proc.check_output('7za {cmd_str}'.format(cmd_str=cmd_str), shell=True)
+    return proc.check_output('{bin} {cmd_str}'.format(bin=bin_path, cmd_str=cmd_str), shell=True)
 
 
-def full_backup_dir(abs_dir_path, save_path='.', strftime='%Y%m%d-%H%M%S', compression_level=3):
-    """Creates a full compressed backup of a folder using 7zip and tar.
+def full_7z_dir(abs_dir_path, save_path='.', strftime='%Y%m%d-%H%M%S', compression_level=3):
+    """Creates a full compressed backup of a folder using 7zip.
     Ideally should be used when backing up a folder for the first time
 
     Parameters
     ----------
-    abs_dir_path : path
+    abs_dir_path : str
         Absolute path to the folder to be compressed
     save_path : path
         Save path of the base archive
@@ -36,17 +51,17 @@ def full_backup_dir(abs_dir_path, save_path='.', strftime='%Y%m%d-%H%M%S', compr
     return run_7za(cmd_str)
 
 
-def diff_backup_dir(abs_dir_path, base_archive_filepath, save_path='.', strftime='%Y%m%d-%H%M%S', compression_level=3):
-    """Creates a differetial compressed backup of a folder using 7zip and tar.
+def diff_7z_dir(abs_dir_path, base_archive_filepath, save_path='.', strftime='%Y%m%d-%H%M%S', compression_level=3):
+    """Creates a differential compressed backup of a folder using 7zip.
     Must have a full backup in place already before a diff backup can be created.
 
     Parameters
     ----------
-    abs_dir_path : path
+    abs_dir_path : str
         Absolute path of the folder being differentially backed-up
-    base_archive_path : filename
+    base_archive_filepath : filename
         File path of the 7zip base (full) archive
-    save_path : path
+    save_path : str
         Save path of the base archive
     strftime : str
         Date and time string formatting rules
@@ -59,25 +74,150 @@ def diff_backup_dir(abs_dir_path, base_archive_filepath, save_path='.', strftime
     int
     """
     folder_name = os.path.split(abs_dir_path)[-1]
-    timestamp = datetime.datetime.now().strftime(strftime)
+    timestamp = str(datetime.datetime.now().strftime(strftime))
     cmd_str = 'u {base_archive_filepath} -ms=off -mx={level} -t7z -u- ' \
-              '-up0q3r2x2y2z0w2\!{save_path}/{folder_name}.{timestamp}.diff.7z {abs_dir_path}'.format(
-        base_archive_filepath=base_archive_filepath, level=compression_level, save_path=save_path,
-        folder_name=folder_name, timestamp=timestamp, abs_dir_path=abs_dir_path)
+              '-up0q3r2x2y2z0w2\!{save_path}/{folder_name}.{timestamp}.diff.7z {abs_dir_path}' \
+        .format(base_archive_filepath=base_archive_filepath, level=compression_level, save_path=save_path,
+                folder_name=folder_name, timestamp=timestamp, abs_dir_path=abs_dir_path)
     return run_7za(cmd_str)
 
 
-def batch_full_backup(abs_paths, save_path='.', strftime='%Y%m%d-%H%M%S', log=True):
+# gnu-tar based backup functions
+
+def run_gnu_tar(cmd_str: str, bin_path='gtar'):
+    """
+
+    Parameters
+    ----------
+    cmd_str
+    bin_path
+
+    Returns
+    -------
+
+    """
+    return proc.check_output('{bin} {cmd_str}'.format(bin=bin_path, cmd_str=cmd_str), shell=True)
+
+
+def full_tar_dir(abs_dir_path, save_path='.', strftime='%Y%m%d-%H%M%S', compression_level=0):
+    """
+
+    Parameters
+    ----------
+    abs_dir_path
+    save_path
+    strftime
+    compression_level
+
+    Returns
+    -------
+
+    """
+    base_path, folder_name = os.path.split(abs_dir_path)
+    timestamp = str(datetime.datetime.now().strftime(strftime))
+    cmd_str = '-c{compress}vf {save_path}/{folder_name}.{timestamp}.base.tar{compress_ext} ' \
+              '-g {save_path}/{folder_name}.{timestamp}.base.snar -C {base_path} {folder_name}' \
+        .format(compress='z' if compression_level > 0 else '', compress_ext='.gz' if compression_level > 0 else '',
+                save_path=save_path, folder_name=folder_name, timestamp=timestamp, base_path=base_path)
+    print(cmd_str)
+    # return run_gnu_tar(cmd_str)
+
+
+def diff_tar_dir(abs_dir_path, base_archive_filepath, save_path='.', strftime='%Y%m%d-%H%M%S', compression_level=0,
+                 deleted_files_list='.deleted_list'):
+    """
+
+    gnu-tar can detect new files or changes but not remove deleted files
+    create anti-file list to instruct deletion of files compared to the original
+
+    Parameters
+    ----------
+    abs_dir_path
+    base_archive_filepath
+    save_path
+    strftime
+    compression_level
+    deleted_files_list
+
+    Returns
+    -------
+
+    """
+    start_wd = os.getcwd()
+    base_path, folder_name = os.path.split(abs_dir_path)
+    timestamp = str(datetime.datetime.now().strftime(strftime))
+
+    # copy base.snar to {timestamp}.snar
+    archive_base_path, archive_name = os.path.split(base_archive_filepath)
+    base_snar_path = os.path.join(archive_base_path, '{}.base.snar'.format(re.search('(^.+)\.base\.tar', archive_name)))
+    diff_snar_path = os.path.join(archive_base_path, '{name}.{timestamp}.diff.snar'.format(
+        name=re.search('(^.+)\.[0-9\-]+\.base\.tar', archive_name), timestamp=timestamp))
+    print(base_snar_path, diff_snar_path)
+    # shutil.copy(base_snar_path, diff_snar_path)
+
+    # list files currently present in the directory
+    os.chdir(base_path)
+    current_filelist = list()
+    for path, dirs, files in os.walk(folder_name):
+        current_filelist.append(path)
+        for f in files:
+            current_filelist.append(os.path.join(path, f))
+
+    # list files in base backup
+    base_filelist = list()
+    with tar.open(base_archive_filepath, 'r') as f:
+        for path in f.getnames():
+            str_match = re.search('^.*({0}.*?)/*$'.format(folder_name), path).group(1)  # matches the folder name first
+            print(str_match)
+            base_filelist.append(str_match)
+
+    # list files in base backup and find files not present anymore
+    # save files to delete as .deleted_list
+    with open(os.path.join(abs_dir_path, deleted_files_list), 'w') as f:
+        for path in [x for x in base_filelist if x not in set(current_filelist)]:
+            print(path, file=f)
+
+    # perform level-1 backup (differential)
+    pass
+
+    os.chdir(start_wd)
+
+
+def batch_full_backup(abs_paths, function=full_7z_dir, save_path='.', strftime='%Y%m%d-%H%M%S', log=True):
+    """Run batch backup for the first time
+
+    Parameters
+    ----------
+    abs_paths : str
+    function : function
+    save_path : str
+    strftime : str
+    log : bool
+
+    """
     timestamp = datetime.datetime.now().strftime(strftime)
     with open(os.path.join(save_path, 'full_backup_log.{timestamp}.txt'.format(timestamp=timestamp)), 'a') as f:
         for path in abs_paths:
-            print(full_backup_dir(path, save_path=save_path, strftime=strftime), file=f)
+            print(function(path, save_path=save_path, strftime=strftime), file=f)
             print('Compressed {}'.format(path))
 
 
-def batch_diff_backup(abs_paths, base_archive_abs_paths, save_path='.', strftime='%Y%m%d-%H%M%S', log=True):
+def batch_diff_backup(abs_paths, base_archive_abs_paths, function=diff_7z_dir, save_path='.', strftime='%Y%m%d-%H%M%S',
+                      log=True):
+    """Run differential batch backup
+
+    Parameters
+    ----------
+    abs_paths : str
+    base_archive_abs_paths : str
+    function : function
+    save_path : str
+    strftime : str
+    log : bool
+
+    """
     timestamp = datetime.datetime.now().strftime(strftime)
     with open(os.path.join(save_path, 'diff_backup_log.{timestamp}.txt'.format(timestamp=timestamp)), 'a') as f:
         for path, archive_path in zip(abs_paths, base_archive_abs_paths):
-            print(diff_backup_dir(path, archive_path, save_path=save_path, strftime=strftime), file=f)
+            print(function(path, archive_path, save_path=save_path, strftime=strftime), file=f)
             print('Differential of {}'.format(path))
--- zipdir/utils.py
+++ zipdir/utils.py
@@ -1,8 +1,19 @@
 import os
 import re
 
+
 def get_marked_dirs(dir_path, mark_filename='.compress_dir'):
     """Get the absolute path of marked directories in a directory tree
+
+    Parameters
+    ----------
+    dir_path : str
+    mark_filename : str
+
+    Returns
+    -------
+    str
+
     """
     marked_abs_paths = list()
     for root, dirs, files in os.walk(dir_path):
@@ -14,6 +25,12 @@ def get_marked_dirs(dir_path, mark_filename='.compress_dir'):
 
 def remove_all_marks(dir_path, mark_filename='.compress_dir'):
     """Remove all marks in a directory tree
+
+    Parameters
+    ----------
+    dir_path : str
+    mark_filename : str
+
     """
     for root, dirs, files in os.walk(dir_path):
         # path = root.split('/')
@@ -24,16 +41,47 @@ def remove_all_marks(dir_path, mark_filename='.compress_dir'):
 
 def mark_dirs(list_of_abs_paths: list, mark_filename='.compress_dir'):
     """Marks each directory in the list for compression
+
+    Parameters
+    ----------
+    list_of_abs_paths
+    mark_filename
+
     """
     for path in list_of_abs_paths:
         print('Marking {}'.format(path))
         open('{}/{}'.format(path, mark_filename), 'a').close()
 
+
 def parse_dirs_file(fh):
+    """
+
+    Parameters
+    ----------
+    fh : file
+
+    Returns
+    -------
+    list
+
+    """
     return [line.replace('\n', '') for line in fh.readlines()]
 
 
 def mark_all_top_subdirs(root_dir, mark_filename='.compress_dir', exclude='^\.'):
+    """Mark top subdirectories for compression
+
+    Parameters
+    ----------
+    root_dir
+    mark_filename
+    exclude
+
+    Returns
+    -------
+    list
+
+    """
     assert os.path.isabs(root_dir)
     abs_paths = list()
     exclude_path = re.compile(str(exclude))
@@ -47,6 +95,19 @@ def mark_all_top_subdirs(root_dir, mark_filename='.compress_dir', exclude='^\.')
 
 
 def mark_all_end_subdirs(root_dir, mark_filename='.compress_dir', exclude='^\.'):
+    """Mark end subdirectories for compression
+
+    Parameters
+    ----------
+    root_dir
+    mark_filename
+    exclude
+
+    Returns
+    -------
+    list
+
+    """
     assert os.path.isabs(root_dir)
     abs_paths = list()
     exclude_path = re.compile(str(exclude))
@@ -60,6 +121,20 @@ def mark_all_end_subdirs(root_dir, mark_filename='.compress_dir', exclude='^\.')
 
 
 def mark_subdirs_gt(root_dir, maxcount=100, mark_filename='.compress_dir', exclude='^\.'):
+    """Mark subdirectories when the number of its contents are greater than a specified number
+
+    Parameters
+    ----------
+    root_dir
+    maxcount
+    mark_filename
+    exclude
+
+    Returns
+    -------
+    list
+
+    """
     assert os.path.isabs(root_dir)
     abs_paths = list()
     exclude_path = re.compile(str(exclude))
@@ -69,3 +144,4 @@ def mark_subdirs_gt(root_dir, maxcount=100, mark_filename='.compress_dir', exclu
                 if os.path.isdir(root):
                     abs_paths.append(root)
     mark_dirs(abs_paths, mark_filename=mark_filename)
+    return abs_paths
